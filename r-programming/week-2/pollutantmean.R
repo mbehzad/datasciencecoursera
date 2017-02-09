47085a37623382c6c5c5320701d0dbb33dd6a023
mbehzad <mbehzad@pro-vision.de>
2017-02-09 16:30:39 +0100

    [r-programming] week 2: adds test data

---
+++ r-programming/week-2/pollutantmean.R
@@ -0,0 +1,22 @@
+## calculate mean of the pollutant
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  ## empty table
+  data <- c();
+  for (i in id) {
+    ## eventually the leading '00' will be added
+    filename <- i;
+    if (i < 10) {
+      filename <- paste("00" , filename, sep = "");
+    }
+    else if (i < 100) {
+      filename <- paste("0" , filename, sep = "");
+    }
+    
+    path <- paste(directory, "/", filename , ".csv", sep = "");
+    ## add data to table
+    data <- rbind(data, read.csv(path));
+  }
+  
+  ## return mean
+  mean(data[[pollutant]], na.rm = TRUE);
+}
