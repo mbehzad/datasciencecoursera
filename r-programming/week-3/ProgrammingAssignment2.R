ebbb8f2eb0ef0f3543e3d2d43984e99b667bd39d
mbehzad <m_online_net@yahoo.com>
2017-02-12 11:29:32 +0100

    solve assignment
    adds functionality to calculate and cache the matrix inverse

---
+++ r-programming/week-3/ProgrammingAssignment2.R
@@ -0,0 +1,35 @@
+## Functions to generate a class to retrive the inverse of a matrix
+## with a build-in cache
+
+## the class with api for setting & getting the matrix and its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+## helper function to return the inverse of the matrix
+## or retrive the cached value if exists
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
+}
